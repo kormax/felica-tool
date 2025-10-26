7c4f3f62b5f2bd7f6cbc31a6312713d7cb78a250
kormax <3392860+kormax@users.noreply.github.com>
2025-10-26 20:44:01 +0200

    Add naming for more systems and nodes

--- app/src/main/java/com/kormax/felicatool/util/NodeNaming.kt
+++ app/src/main/java/com/kormax/felicatool/util/NodeNaming.kt
@@ -11,11 +11,20 @@ object NodeNaming {
     /** Map of system codes to their known names. */
     private val systemNames =
         mapOf(
-            "88B4" to "FeliCa Lite",
-            "12FC" to "NDEF Type 3 Tag",
             "0003" to "CJRC Standard",
-            "86A7" to "Suica",
+            "12FC" to "NDEF Type 3 Tag",
+            "8005" to "Shenzhen Tong",
             "8008" to "Octopus",
+            "80DE" to "IruCa",
+            "8194" to "RapiCa",
+            "865E" to "SAPICA",
+            "86A7" to "Suica",
+            "88B4" to "FeliCa Lite",
+            "8BDC" to "Rakuten Edy",
+            "8FC1" to "OKICA",
+            "9027" to "Kumamon IC",
+            "9099" to "emica",
+            "927A" to "Hayakaken",
             "FE00" to "FeliCa Networks Common Area",
             "FE0F" to "Osaifu Keitai Container",
         )
@@ -33,7 +42,14 @@ object NodeNaming {
                     "8155BF56" to "Nanaco",
                     "C067FF68" to "WAON root",
                     "C167FF68" to "WAON",
-                )
+                ),
+            "0300" to
+                mapOf(
+                    "c01cff1d" to "Pasmo root",
+                    "001eff1e" to "TOICA root",
+                    "40247F25" to "Hayakaken root",
+                    "8020BF21" to "Kitaca root",
+                ),
         )
 
     /** Map of system code -> area code -> service code -> service name. */
@@ -46,9 +62,9 @@ object NodeNaming {
                         mapOf("0B11" to "Edy ID", "1713" to "Edy balance", "0F17" to "Edy log"),
                     "4139FF39" to
                         mapOf(
-                            "4B39" to "Pocket Capabilities",
-                            "8B39" to "Pocket Directory",
-                            "C939" to "Pocket Data",
+                            "4B39" to "Pocket capabilities",
+                            "8B39" to "Pocket directory",
+                            "C939" to "Pocket data",
                         ),
 
                     // nanaco area services
@@ -68,10 +84,56 @@ object NodeNaming {
                             "4B68" to "WAON points",
                         ),
                 ),
+            // Sources:
+            // 1. http://jennychan.web.fc2.com/format/suica.html
             "0003" to
                 mapOf(
                     // Suica transaction services (area not specified in scan, using system-level)
-                    "" to mapOf("0F09" to "CJRC log")
+                    "" to
+                        mapOf(
+                            "0F09" to "CJRC usage history",
+                            "4B18" to "CJRC commuter pass info",
+                            "8B00" to "CJRC attributes",
+                            "8B18" to "Suica mobile capability container",
+                            "8F10" to "CJRC ticket gate history",
+                            "CB10" to "CJRC entrance station info",
+                        )
+                ),
+            "8008" to mapOf("" to mapOf("1701" to "Octopus balance")),
+            // IRUCA re-uses CJRC format
+            "80DE" to
+                mapOf(
+                    "" to
+                        mapOf(
+                            "0F09" to "IruCa usage history",
+                            "4B18" to "IruCa commuter pass info",
+                            "8B00" to "IruCa attributes",
+                            "8F10" to "IruCa ticket gate history",
+                            "CB10" to "IruCa entrance station info",
+                        )
+                ),
+            // Sources:
+            // 1. http://jennychan.web.fc2.com/format/rapica.html
+            "8194" to
+                mapOf(
+                    "" to
+                        mapOf(
+                            "0B00" to "RapiCa issuance information",
+                            "4B00" to "RapiCa attirubte info",
+                            "8F00" to "RapiCa usage history",
+                            "CF00" to "RapiCa balance recharge history",
+                        )
+                ),
+            // SAPICA re-uses CJRC format
+            "865E" to
+                mapOf(
+                    "" to
+                        mapOf(
+                            "0F09" to "SAPICA usage history",
+                            "8B00" to "SAPICA attributes",
+                            "8F10" to "SAPICA ticket gate history",
+                            "CB10" to "SAPICA entrance station info",
+                        )
                 ),
             "88B4" to
                 mapOf(
